154fb34a949fcf1fb18abe8622fda7367b412cb1
terry truong <terrytruong71@gmail.com>
2020-11-09 23:08:26 -0500

    TEST

---
+++ TestingAplication/TestingAplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printf("Hello");
+        }
+
+        private static void printf(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
